4b216b375b0c29f4e963b28ed360aba99ed779c9
Priscila Ramonna Da silva Pires <princyrpiress@gmail.com>
2025-10-01 14:55:31 -0300

    Atualizando agendamento e envio de e-mails

--- routes/bookings.js
+++ routes/bookings.js
@@ -20,7 +20,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER || 'clarissaalcantara064@gmail.com',
-    pass: process.env.EMAIL_PASS || 'oirgbfdyeupamfnx'
+    pass: process.env.EMAIL_PASS || 'nyuchxrlkfxvwvtp'
   }
 });
 
